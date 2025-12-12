699c5d017ced7cd646920e3b50d8a760d6181a7b
4479 <142999203+4479cantcode@users.noreply.github.com>
2025-12-12 13:17:12 +0100

    Update MainHub.lua with supported games list
    Added support information for currently supported games.

---
+++ Script/MainHub.lua
@@ -0,0 +1,7 @@
+loadstring(game:HttpGet("https://api.junkie-development.de/api/v1/luascripts/public/61f9e4e53e0d639db1122d02cc47aec30905c362808b723ee3bddbaad6721c9c/download"))()
+--[[ Currently Supported:
+    - Slap Battles (KEYLESS)
+    - Doors (KEYLESS)
+    - Rivals (KEYLESS)
+join discord  -- https://discord.gg/h4KPjjn2Sv
+]]
